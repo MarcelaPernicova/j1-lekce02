69baf9c90a63953ebf09e5c288957483ac0afa98
Marcela Pernicová <marcela.pernicova4@gmail.com>
2023-02-14 20:40:47 +0100

    žofka kreslí domek a částečně prase

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -6,8 +6,28 @@ public class HlavniProgram {
     private Turtle zofka;
     public void start() {
         zofka = new Turtle();
-
-        //TODO tady bude kód
+        zofka.setLocation(90,400);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+        zofka.turnRight(60);
+        zofka.move(100);
+        zofka.turnRight(60);
+        zofka.move(100);
+        zofka.turnRight(150);
+        zofka.move(170);
+        zofka.turnLeft(90);
+        zofka.move(200);
+        zofka.turnLeft(90);
+        zofka.move(170);
+        zofka.turnLeft(90);
+        zofka.move(200);
+        zofka.setLocation(400,380);
+        zofka.move(90);
+        zofka.turnLeft(120);
+        zofka.move(110);
+        zofka.turnLeft(130);
+        zofka.move(110);
     }
 
     public static void main(String[] args) {
